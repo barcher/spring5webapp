eddecde5ca336e663ab9ce8d9f9539dbb810e64c
Bryce Archer <bryce.charles.archer@gmail.com>
2020-07-27 05:27:06 -0700

    Add Publisher relationships

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -36,19 +36,26 @@ public class BootstrapData implements CommandLineRunner {
         Book ddd = new Book("Domain Driven Design", "123456");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "123123");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in bootstrap.");
         System.out.println("Number of books " + bookRepository.count());
+        System.out.println("Number of books with publisher " + publisher.getBooks().size());
     }
 }
